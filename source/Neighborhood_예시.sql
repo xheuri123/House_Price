afc60902c4953eed61f62ecc2caaeab4a9fd2c32
jin1302 <powerhagol@gmail.com>
2026-02-05 11:27:16 +0900

    신규 업로드 !

---
+++ source/Neighborhood_예시.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE project;
+USE project;
+DESCRIBE house_price;
+
+SELECT * FROM house_price;
+
+# [MADE IN AI]
+SELECT @global_avg := AVG(saleprice) FROM house_price;
+WITH stats AS ( 
+  SELECT Neighborhood, AVG(saleprice) avg_price 
+  FROM house_price GROUP BY Neighborhood 
+)
+SELECT 
+  Neighborhood, 
+  ROUND(avg_price,-1) 평균가, 
+  ROUND(avg_price - g.global_avg, 0) as 평균_차이, 
+  CONCAT(ROUND(100*(avg_price/g.global_avg-1)-1),'%') 프리미엄율, 
+  # (윈도우 함수 행의 백분위 수를 계산)
+  ROUND(100 - PERCENT_RANK() OVER (ORDER BY avg_price DESC) * 100) as 지역_프리미엄_점수
+  # (윈도우 함수)
+  , NTILE(4) OVER(ORDER BY avg_price DESC) 사분위 
+# stats -> WITH stats
+FROM stats, (SELECT AVG(avg_price) global_avg FROM stats) g;
+
+
+
